827aec07f42290c8b2eab6a4077926f9b6108f9c
Kirill Kayumov <kirill.kayumov@flatstack.com>
2016-09-08 18:55:01 +0300

    Update apiTarget

--- config/env/production.js
+++ config/env/production.js
@@ -1,6 +1,6 @@
 module.exports = {
   target: 'http://react-base.herokuapp.com',
-  apiTarget: 'http://react-base-api.herokuapp.com',
+  apiTarget: 'http://social-pdp-api.herokuapp.com/v1',
   storageKey: 'user_session',
   session: {
     tokenKey: 'authentication_token',
